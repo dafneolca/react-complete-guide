14313aa79488893613a21092759453b7d26bef74
dafneolca <olca.dafne@gmail.com>
2019-08-16 14:39:10 +0200

    converted to useState() Hook for State manipulation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+import Person from './Person/Person'
+
+const app = props => {
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      { name: "Dafne", age: 31 },
+      { name: "Elfi", age: 59 },
+      { name: "Srini", age: 60 }
+    ]
+  })
+
+  const [otherState, newOtherState] = useState('some other value')
+
+  console.log(personsState, otherState)
+
+  const switchNameHandler = () => {
+    console.log("okkkk")
+    setPersonsState({
+      persons: [
+        { name: "Dafne DENIZ", age: 31 },
+        { name: "Elfi", age: 59 },
+        { name: "Srini", age: 600 }
+      ]
+    })
   }
+
+  return (
+    <div className="App">
+      <h1>Hi, I'm Dafne ;-)</h1>
+      <h3>is this really working?</h3>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>Special *CaSe*</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+    </div>
+  );
+
 }
 
-export default App;
+export default app;
---
+++ src/Person/Person.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const person = (props) => {
+  return (
+    <div>
+      <p>I'm {props.name}.. a person! I am {props.age} years old. - {props.children}</p>
+
+    </div>
+  )
+
+}
+
+export default person;
